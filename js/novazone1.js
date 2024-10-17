38cf1a4dca51779ca23b2ebb1f2c473a70116141
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2024-10-17 18:06:10 +0100

    Add NovaZone1 fucking with body style which might break things

---
+++ js/novazone1.js
@@ -0,0 +1,27 @@
+let images = [
+    'images/novazone1 (1).png'];
+let currentImageIndex = 0;
+
+// Preload images and ensure they are fully loaded
+let preloadedImages = [];
+images.forEach((src, index) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+        console.log(`Image ${index + 1} loaded: ${src}`);
+        preloadedImages.push(img);
+    };
+});
+
+function changeBackground() {
+    // Only change the background if all images are preloaded
+    if (preloadedImages.length === images.length) {
+        //console.log(`Changing background to: ${images[currentImageIndex]}`);
+        document.body.style.backgroundImage = "url(\"" + images[currentImageIndex] + "\")";
+
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+}
+//document.body.style.backgroundImage = "url('images/scrapforge (2).png')";
+document.body.style.backgroundImage = "url(\"" + images[currentImageIndex] + "\")";
+setInterval(changeBackground, 6000); // Change image every 5 seconds
