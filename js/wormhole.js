d5a7e1b205b26c8d5cdf9d84e935747ede1860d3
LordSiron <149695698+Lordet-Iron@users.noreply.github.com>
2024-10-18 14:28:03 +0100

    Added wormhole

---
+++ js/wormhole.js
@@ -0,0 +1,26 @@
+let images = [
+    'images/wormhole (1).png'];
+let currentImageIndex = 0;
+
+// Preload images and ensure they are fully loaded
+let preloadedImages = [];
+images.forEach((src, index) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+        console.log(`Image ${index + 1} loaded: ${src}`);
+        preloadedImages.push(img);
+    };
+});
+
+function changeBackground() {
+    // Only change the background if all images are preloaded
+    if (preloadedImages.length === images.length) {
+        //console.log(`Changing background to: ${images[currentImageIndex]}`);
+        document.body.style.backgroundImage = "url(\"" + images[currentImageIndex] + "\")";
+
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+}
+
+setInterval(changeBackground, 6000); // Change image every 5 seconds
