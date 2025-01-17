df9cdffda6cbccc95d0537f9477fcff0f486dcfd
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-01-17 16:29:01 +0000

    Added Project Dragon

---
+++ js/projectdragon.js
@@ -0,0 +1,30 @@
+let images = [
+    'images/projectdragon (1).png',
+    'images/projectdragon (2).png',
+    'images/projectdragon (3).png',
+    'images/projectdragon (4).png'];
+let currentImageIndex = 0;
+
+// Preload images and ensure they are fully loaded
+let preloadedImages = [];
+images.forEach((src, index) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+        console.log(`Image ${index + 1} loaded: ${src}`);
+        preloadedImages.push(img);
+    };
+});
+
+function changeBackground() {
+    // Only change the background if all images are preloaded
+    if (preloadedImages.length === images.length) {
+        //console.log(`Changing background to: ${images[currentImageIndex]}`);
+        document.body.style.backgroundImage = "url(\"" + images[currentImageIndex] + "\")";
+
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+}
+//document.body.style.backgroundImage = "url('images/scrapforge (2).png')";
+
+setInterval(changeBackground, 6000); // Change image every 5 seconds
